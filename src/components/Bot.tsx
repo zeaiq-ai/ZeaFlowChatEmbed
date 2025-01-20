4306b86b7ae6ae911a57eb8d20a40ec4b808da3f
abhilash <abhilashk@zealogics.com>
2025-01-20 11:35:39 +0400

    REMOVE BRANDING

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -1619,12 +1619,12 @@ export const Bot = (botProps: BotProps & { class?: string }) => {
               />
             )}
           </div>
-          <Badge
+          {/* <Badge
             footer={props.footer}
             badgeBackgroundColor={props.badgeBackgroundColor}
             poweredByTextColor={props.poweredByTextColor}
             botContainer={botContainer}
-          />
+          /> */}
         </div>
       </div>
       {sourcePopupOpen() && <Popup isOpen={sourcePopupOpen()} value={sourcePopupSrc()} onClose={() => setSourcePopupOpen(false)} />}
