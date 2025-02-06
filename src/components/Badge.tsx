f1df743dcfd338bb7c0f3aedf9917174160669a0
abhilash <abhilashk@zealogics.com>
2025-02-06 12:11:27 +0400

    remove branding

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -48,7 +48,7 @@ export const Badge = (props: Props) => {
             'background-color': props.badgeBackgroundColor ?? '#ffffff',
           }}
         >
-          {props.footer?.text ?? 'Powered by'}
+          {/* {props.footer?.text ?? 'Powered by'} */}
           {/* <a
             ref={liteBadge}
             href={props.footer?.companyLink ?? 'https://zealogics.com'}
